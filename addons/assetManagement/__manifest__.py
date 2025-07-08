6a1ff59f10b1d7fb532447994beebce57a2c167a
mazkar27 <mazkar27@gmail.com>
2025-07-08 17:09:54 +0700

    update

--- addons/assetManagement/__manifest__.py
+++ addons/assetManagement/__manifest__.py
@@ -7,18 +7,14 @@
     'depends': ['base','mail','stock'],
     'data': [
         'views/asset_condition_month_views.xml',
-        # 'views/asset_condition_month_line_views.xml',
         'views/asset_condition_month_done_views.xml',
         'views/menu.xml',
         'views/asset_item_views.xml',
         'views/user_group_views.xml',
         'views/approval_route_line_menu.xml',
-        # 'views/asset_menu_views.xml',
-        'data/module_category.xml',
         'data/approval_group_and_route.xml',
-        'data/fix_warehouse.xml',
         'security/ir.model.access.csv',
-        'data/override_stock_data.xml',
+      
     ],
     'application': True,
 }
--- addons/assetManagement/hooks.py
+++
@@ -1,2 +0,0 @@
-def pre_init_hook(cr):
-    cr.execute("DELETE FROM stock_warehouse WHERE code = 'WH' AND company_id = 1;")
